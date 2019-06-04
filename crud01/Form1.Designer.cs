2090e7051cf81723eb6de5f87c6225e5c8c461bc
Thiago Moura <thiago.mouraluzia@gmail.com>
2019-06-03 23:32:14 -0300

    PARTE 1. CONEXAO E READ - Criando UMA conexao com o banco de daos e uma aplicação simples com listagem dos arquivos contidos no banco de dados

---
+++ crud01/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace crud01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnVerResultados = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::crud01.Properties.Resources._20160918052124;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ligação a uma Base de Dados SQL-Server em C#";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 216);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 193);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btnVerResultados
+            // 
+            this.btnVerResultados.Location = new System.Drawing.Point(241, 415);
+            this.btnVerResultados.Name = "btnVerResultados";
+            this.btnVerResultados.Size = new System.Drawing.Size(126, 23);
+            this.btnVerResultados.TabIndex = 4;
+            this.btnVerResultados.Text = "Ver Resultados";
+            this.btnVerResultados.UseVisualStyleBackColor = true;
+            this.btnVerResultados.Click += new System.EventHandler(this.btnVerResultados_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.Controls.Add(this.btnVerResultados);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Ligação C#  SQL-Server";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnVerResultados;
+    }
+}
+
---
+++ crud01/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace crud01
+{
+    public partial class Form1 : Form
+    {
+        //1ºcriar uma conexão
+        private SqlConnection mConn;
+        private SqlDataAdapter mAdapter;
+        private DataSet mDataSet;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerResultados_Click(object sender, EventArgs e)
+        {
+            //2ºdefine o dataset
+            mDataSet = new DataSet();
+
+            //3ºdefine a string de conexao e criar a conexao
+            mConn = new SqlConnection("server=desktop-easat3o\\sqlexpress; Database=crud01; User Id=sa; Password=bwy6932guz");
+            mConn.Open();
+
+            //4º Verifica se a conexao esta aberta 
+            if (mConn.State == ConnectionState.Open)
+            {
+                //5ºcria um adapter utilizando a instrução sql para acessar a tabela dados
+                mAdapter = new SqlDataAdapter("SELECT * FROM dados ORDER BY ID", mConn);
+
+                //6ºPreenche o dataSet atraves do data adapter
+                mAdapter.Fill(mDataSet, "dados");
+
+                //7ºatribui a resultado a propriedade DataSource do DataridView
+                dataGridView1.DataSource = mDataSet;
+                dataGridView1.DataMember = "dados";
+            }
+        }
+    }
+}
