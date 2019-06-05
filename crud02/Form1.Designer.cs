20b024c962449f57013dc09edd7bb72d77c986d9
Thiago Moura <thiago.mouraluzia@gmail.com>
2019-06-04 22:10:15 -0300

    PARTE 2. CONEXAO COM O BANCO DE DADOS, INSERT E READ - Foi criada uma conexao com o banco de dados, e feita sua conexao e a inserção de dados no banco, quando o dado e salvo no banco, o data grid e atualizado e lista o novo registro, alem de exibir uma message box de sucesso quando o cadastro é efetuado.

---
+++ crud02/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace crud02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnVisualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::crud02.Properties.Resources._01506989481;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(347, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(347, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(243, 461);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 7;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Iserir Dados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Titulo";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(15, 353);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(188, 20);
+            this.txtTitulo.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descrição";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(15, 392);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(303, 63);
+            this.txtDescricao.TabIndex = 6;
+            // 
+            // btnVisualizar
+            // 
+            this.btnVisualizar.Location = new System.Drawing.Point(243, 311);
+            this.btnVisualizar.Name = "btnVisualizar";
+            this.btnVisualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnVisualizar.TabIndex = 2;
+            this.btnVisualizar.Text = "Visualizar";
+            this.btnVisualizar.UseVisualStyleBackColor = true;
+            this.btnVisualizar.Click += new System.EventHandler(this.btnVisualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 496);
+            this.Controls.Add(this.btnVisualizar);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnVisualizar;
+    }
+}
+
---
+++ crud02/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace crud02
+{
+    public partial class Form1 : Form
+    {
+        //1ºCriar a conexao
+        private SqlConnection mConn;
+        private SqlDataAdapter mAdapter;
+        private DataSet mDataset;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //CRIADO POR ULTIMO E POR CONTA - Chamando o metodo de mostrarResultados no datagridview
+            mostrarResultado();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            // Início da Conexão com indicação de qual o servidor, nome de base de dados e utilizar
+
+            /* É aconselhável criar um utilizador com password. Para acrescentar a password é somente
+            necessário acrescentar o seguinte código a seguir ao uid=root;password=xxxxx*/
+            mConn = new SqlConnection("server=desktop-easat3o\\sqlexpress; Database=crud02; User Id=sa; Password=bwy6932guz");
+
+            //abrir conexao
+            mConn.Open();
+
+            //query sql
+            SqlCommand comando = new SqlCommand("INSERT INTO tabela_dados (titulo, descricao) VALUES('" + txtTitulo.Text + "','" + txtDescricao.Text + "')", mConn);
+                                                
+            //executa a query sql
+            comando.ExecuteNonQuery();
+
+            //fecha conexao
+            mConn.Close();
+
+            //messagem de sucesso
+            MessageBox.Show("Gravado com sucesso", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //CRIADO POR ULTIMO E POR CONTA - Limpando os campos apos os dados serem inseridos
+            txtTitulo.Text = "";
+            txtDescricao.Text = "";
+
+            // Método criado para que quando o registo é gravado, automaticamente a dataGridView efectue um "Refresh"
+            mostrarResultado();
+        }
+
+        //metodo para visualização de dados
+        private void mostrarResultado()
+        {
+            mDataset = new DataSet();
+            mConn = new SqlConnection("server=desktop-easat3o\\sqlexpress; Database=crud02; User Id=sa; Password=bwy6932guz");
+            mConn.Open();
+
+            //criar um adapter utilizando a instrução sql  para aceder á tabela
+            mAdapter = new SqlDataAdapter("SELECT * FROM tabela_dados ORDER BY id", mConn);
+
+            //preenche o dataset atravez do dataAdapter
+            mAdapter.Fill(mDataset, "tabela_dados");
+
+            //atribui o resultado a proriedade datasource da datagridview
+            dataGridView1.DataSource = mDataset;
+            dataGridView1.DataMember = "tabela_dados";
+
+        }
+
+        private void btnVisualizar_Click(object sender, EventArgs e)
+        {
+            //chamando o metodo de visualizar os registros no datagrid
+            mostrarResultado();
+        }
+    }
+}
